826e0a38ebd7e002c74c9c6807f40104b4de9b86
Rajini Wijayanayake <rajiniwijay@gmail.com>
2020-07-29 07:26:36 -0700

    Initial Commit

---
+++ Moodify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moodify
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Rajini, Bea, & Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
