70b64622812d51b8e8cdbcf5ee25d82fd4522213
Beatrix Boer <beatrixboer@icloud.com>
2020-07-30 17:38:21 -0400

    forth commit

--- Moodify/ThirdViewController.swift
+++ Moodify/ThirdViewController.swift
@@ -10,53 +10,53 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
-    @IBOutlet weak var emotion: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        func veryHappy(_ sender: Any) {
-                choiceSelected (choice : 0)
-            }
-            
-        func happy(_ sender: Any) {
-                choiceSelected (choice : 1)
-            }
-            
-        func meh(_ sender: Any) {
-                choiceSelected (choice : 2)
-            }
-            
-        func sad(_ sender: Any) {
-                choiceSelected (choice : 3)
-            }
-            
-        func stressed(_ sender: Any) {
-                choiceSelected (choice : 4)
-            }
-            
-        func angry(_ sender: Any) {
-                choiceSelected (choice : 5)
-            }
-            
+//         func veryHappy(_ sender: Any) {
+//                choiceSelected (choice : 0)
+//            }
+//
+//         func happy(_ sender: Any) {
+//                choiceSelected (choice : 1)
+//            }
+//
+//        func meh(_ sender: Any) {
+//                choiceSelected (choice : 2)
+//            }
+//
+//        func sad(_ sender: Any) {
+//                choiceSelected (choice : 3)
+//            }
+//
+//        func stressed(_ sender: Any) {
+//                choiceSelected (choice : 4)
+//            }
+//
+//        func angry(_ sender: Any) {
+//                choiceSelected (choice : 5)
+//            }
+//
         }
     
-    func choiceSelected (choice : Int) {
-            if choice == 0 {
-                emotion.text = "Your mood was Extremely Happy today! Congratulations! Now let’s reflect on your day: What were three good things that happened throughout your wonderful day? Did you have any successes? It is also good to think about what didn’t go right today. How will you make tomorrow better? (Bonus: Write your thoughts down!)"
-            } else if choice == 1 {
-                emotion.text = "Your mood was Happy today! That’s amazing! Now let’s reflect on your day: What were three good things that happened today? Did you have any big wins today? If so, who was there to celebrate with you? What will you do to make tomorrow just as good or better? (Bonus: Write down your answers!)"
-            } else if choice == 2 {
-                emotion.text = "Your mood was Meh today! You didn’t have anything go specifically wrong or right today. That’s okay! Just remember that some days are better than others and you will never stay in the same place! Now let’s reflect on your day: Try really hard to think about one thing that went well for you today? Were you with anyone when it happened? Were you doing something specific? Try to do it again tomorrow if it made you happy! (Bonus: Take action to make tomorrow better!)"
-            } else if choice == 3 {
-                emotion.text = "Your mood was Sad today! That’s totally okay! Some days don’t go as well as others but no matter what you will always keep moving forward! Now let’s reflect on your day: Although you might think you had a bad day, try really hard to think about at least one good thing that went your way? If something in your day didn’t go as you expected try to think about what could’ve gone differently and what you could do better tomorrow! (Bonus: Get some exercise! Moving can help you sort out your thoughts and reflect!)"
-            } else if choice == 4 {
-               emotion.text = "Your mood was Stressed today! Don’t sweat it! We can’t always control outside factors and sometimes they can mess with our day but we can control ourselves and how we react! Know that you tried your best today and that’s what really matters! Now let’s reflect on your day: What are somethings that you could do to relieve your stress? You could talk to a loved one or take a break from your daily tasks to relax. There is always someone you can talk too! (Bonus: Breath in deeply through your nose and breath out slowly from your mouth!)"
-            } else if choice == 5 {
-               emotion.text = "Your mood was Angry today! It’s okay! You can let your frustration out safely! Now let’s reflect on your day: What made you angry today? Often when we’re upset little things can feel amplified. Take a deep breath and try to stay calm in frustrating situations. Can you fix what you’re angry about? Forgiveness of yourself and others is key. Try going outside and getting fresh air! Take in all the nature and beauty of the world! (Bonus: Take a step back and get a new perspective!)"
-            }
+//    func choiceSelected (choice : Int) {
+//            if choice == 0 {
+//                emotion.text = "Your mood was Extremely Happy today! Congratulations! Now let’s reflect on your day: What were three good things that happened throughout your wonderful day? Did you have any successes? It is also good to think about what didn’t go right today. How will you make tomorrow better? (Bonus: Write your thoughts down!)"
+//            } else if choice == 1 {
+//                emotion.text = "Your mood was Happy today! That’s amazing! Now let’s reflect on your day: What were three good things that happened today? Did you have any big wins today? If so, who was there to celebrate with you? What will you do to make tomorrow just as good or better? (Bonus: Write down your answers!)"
+//            } else if choice == 2 {
+//                emotion.text = "Your mood was Meh today! You didn’t have anything go specifically wrong or right today. That’s okay! Just remember that some days are better than others and you will never stay in the same place! Now let’s reflect on your day: Try really hard to think about one thing that went well for you today? Were you with anyone when it happened? Were you doing something specific? Try to do it again tomorrow if it made you happy! (Bonus: Take action to make tomorrow better!)"
+//            } else if choice == 3 {
+//                emotion.text = "Your mood was Sad today! That’s totally okay! Some days don’t go as well as others but no matter what you will always keep moving forward! Now let’s reflect on your day: Although you might think you had a bad day, try really hard to think about at least one good thing that went your way? If something in your day didn’t go as you expected try to think about what could’ve gone differently and what you could do better tomorrow! (Bonus: Get some exercise! Moving can help you sort out your thoughts and reflect!)"
+//            } else if choice == 4 {
+//               emotion.text = "Your mood was Stressed today! Don’t sweat it! We can’t always control outside factors and sometimes they can mess with our day but we can control ourselves and how we react! Know that you tried your best today and that’s what really matters! Now let’s reflect on your day: What are somethings that you could do to relieve your stress? You could talk to a loved one or take a break from your daily tasks to relax. There is always someone you can talk too! (Bonus: Breath in deeply through your nose and breath out slowly from your mouth!)"
+//            } else if choice == 5 {
+//               emotion.text = "Your mood was Angry today! It’s okay! You can let your frustration out safely! Now let’s reflect on your day: What made you angry today? Often when we’re upset little things can feel amplified. Take a deep breath and try to stay calm in frustrating situations. Can you fix what you’re angry about? Forgiveness of yourself and others is key. Try going outside and getting fresh air! Take in all the nature and beauty of the world! (Bonus: Take a step back and get a new perspective!)"
+//            }
                
            }
-}
+
 
 
